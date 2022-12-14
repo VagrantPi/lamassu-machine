50aad1b25975b59b9c06105e54e220609e59dabd
siiky <github-siiky@net-c.cat>
2022-12-14 12:56:58 +0000

    fix: call `deactivate` on the keyboards and keypads

--- ui/js/docker-app.js
+++ ui/js/docker-app.js
@@ -702,7 +702,7 @@ $(document).ready(function () {
     buttonPressed('blockedCustomerOk');
   });
   var insertBillCancelButton = document.getElementById('insertBillCancel');
-  touchImmediateEvent(insertBillCancelButton, function () {
+  touchImmediateEvent(insertBillCancelButton, null, function () {
     setBuyerAddress(null);
     buttonPressed('cancelInsertBill');
   });
@@ -729,7 +729,7 @@ $(document).ready(function () {
   setupButton('choose-fiat-promo-button', 'insertPromoCode');
 
   var promoCodeCancelButton = document.getElementById('promo-code-cancel');
-  touchImmediateEvent(promoCodeCancelButton, function () {
+  touchImmediateEvent(promoCodeCancelButton, null, function () {
     promoKeyboard.deactivate.bind(promoKeyboard);
     buttonPressed('cancelPromoCode');
   });
@@ -917,29 +917,22 @@ $(document).ready(function () {
   setupButton('facephoto-scan-failed-cancel', 'finishBeforeSms');
   setupButton('facephoto-scan-failed-cancel2', 'finishBeforeSms');
 
-  setupButton('custom-permission-yes', 'permissionCustomInfoRequest');
-  setupButton('custom-permission-no', 'finishBeforeSms');
-  setupImmediateButton('custom-permission-cancel-numerical', 'cancelCustomInfoRequest', function () {
-    customRequirementNumericalKeypad.deactivate.bind(customRequirementNumericalKeypad);
-  });
   setupImmediateButton('email-cancel', 'cancelEmail', function () {
     emailKeyboard.deactivate.bind(emailKeyboard);
     $('#email-input').data('content', '').val('');
     emailKeyboard.setInputBox('#email-input');
   });
+
+  setupButton('custom-permission-yes', 'permissionCustomInfoRequest');
+  setupButton('custom-permission-no', 'finishBeforeSms');
+  setupImmediateButton('custom-permission-cancel-numerical', 'cancelCustomInfoRequest', customRequirementNumericalKeypad.deactivate.bind(customRequirementNumericalKeypad));
   setupImmediateButton('custom-permission-cancel-text', 'cancelCustomInfoRequest', function () {
-    customRequirementTextKeyboard.deactivate.bind(customRequirementTextKeyboard);
+    customRequirementTextKeyboard.deactivate.bind(customRequirementTextKeyboard)();
     $('.text-input-field-1').removeClass('faded').data('content', '').val('');
     $('.text-input-field-2').addClass('faded').data('content', '').val('');
     customRequirementTextKeyboard.setInputBox('.text-input-field-1');
   });
-  setupImmediateButton('custom-permission-cancel-choiceList', 'cancelCustomInfoRequest', function () {});
-
-  setupButton('custom-permission-yes', 'permissionCustomInfoRequest');
-  setupButton('custom-permission-no', 'finishBeforeSms');
-  setupImmediateButton('custom-permission-cancel-numerical', 'cancelCustomInfoRequest', function () {
-    customRequirementNumericalKeypad.deactivate.bind(customRequirementNumericalKeypad);
-  });
+  setupImmediateButton('custom-permission-cancel-choiceList', 'cancelCustomInfoRequest');
 
   setupButton('external-validation-ok', 'finishBeforeSms');
 
@@ -1059,7 +1052,7 @@ function touchImmediateEvent(element, action, callback) {
   // As the same element can have different actions hooked on the same event, this needs to be stored as an array of <action, handler> pairs
 
   // The viewportButtonEventsActive ensures that no repeated events are being added to the element
-  if (element.id.includes('_viewport')) {
+  if (action && element.id.includes('_viewport')) {
     if (!viewportEvents[element.id]) viewportEvents[element.id] = [];
     viewportEvents[element.id].push({ action: action, handler: handler });
   }
@@ -1555,7 +1548,7 @@ function buildCassetteButtonEvents() {
   var fiatButtons = document.getElementById('js-fiat-buttons');
   var lastTouch = null;
 
-  touchImmediateEvent(fiatButtons, function (e) {
+  touchImmediateEvent(fiatButtons, null, function (e) {
     var now = Date.now();
     if (lastTouch && now - lastTouch < 100) return;
     lastTouch = now;
