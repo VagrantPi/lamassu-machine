a7eee8c29629d1a58d8c7bc6ceccd07352de1351
siiky <github-siiky@net-c.cat>
2024-11-20 16:47:32 +0000

    refactor: simplify `calibrate-screen.sh` configuration

--- hardware/system/up4000/grandola/calibrate-screen.sh
+++ hardware/system/up4000/grandola/calibrate-screen.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/env sh
-xrandr -o normal # normal, inverted, left, right
-identityMat='1 0 0 0 1 0 0 0 1'
-leftRotateMat='0 -1 1 1 0 0 0 0 1'
-rightRotateMat='0 1 0 -1 0 1 0 0 1'
-xinput set-prop 'ILITEK ILITEK-TP' --type=float 'Coordinate Transformation Matrix' $identityMat
-xset s off
-xset s noblank
-xset -dpms
+set -e
+
+### CHANGE CONFIGURATION AT THE END OF THE FILE ###
+
+configure_screen() {
+	local rotation="$1"
+	case "${rotation}" in
+		normal) touchRotation='1 0 0 0 1 0 0 0 1';;
+		left) touchRotation='0 -1 1 1 0 0 0 0 1';;
+		right) touchRotation='0 1 0 -1 0 1 0 0 1';;
+		*) echo "Unknown rotation '${rotation}'..."; exit 1;;
+	esac
+	screenRotation="${rotation}"
+
+	xrandr -o "${screenRotation}" \
+		&& xset s off \
+		&& xset s noblank \
+		&& xset -dpms
+}
+
+configure_touch() {
+	local screen="$1"
+	xinput set-prop "${screen}" --type=float 'Coordinate Transformation Matrix' $touchRotation
+}
+
+### CHANGE CONFIGURATION BELOW TO MATCH THE CONNECTED SCREEN ###
+
+# One of: normal, left, right
+configure_screen normal
+# Find the screen name under "Virtual core pointer" of the following command:
+#   xinput list
+configure_touch 'ILITEK ILITEK-TP'
