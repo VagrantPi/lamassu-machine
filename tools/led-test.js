08fb5f5792385858fcc0030e726b6355bf14ff38
siiky <github-siiky@net-c.cat>
2025-04-16 12:43:26 +0100

    feat: add usage message

--- tools/led-test.js
+++ tools/led-test.js
@@ -3,6 +3,10 @@ const ledManager = require('../lib/leds/led-manager')
 const actionEmitter = require('../lib/action-emitter')
 
 const machine = process.argv[2]
+if (!machine) {
+  console.log("Usage: led-test.js MACHINE")
+  process.exit(1)
+}
 
 function emit (subsystem, action) {
   return () => actionEmitter.emit(subsystem, {action})
@@ -22,4 +26,3 @@ Promise.resolve(ledManager.run(machine))
   .then(emit('door', 'doorNotSecured'))
   .then(() => delay(3000))
   .then(emit('door', 'doorSecured'))
-
